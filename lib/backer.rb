0f977051de9f96c5fc01722d33118599605110dc
root <root@DESKTOP-2V0R1D8.localdomain>
2019-09-24 18:55:53 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,11 +4,13 @@ class Backer
   attr_reader :backed_projects
 
   def initialize(name)
+    @name = name
     @backed_projects = []
   end
 
   def back_project(project)
     @backed_projects << project
+    project.backers << self
   end
 
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,15 @@
 class Project
 
-  attr_reader :backers
+  attr_reader :backers, :title
 
-  def initialize
+  def initialize(title)
+    @title = title
     @backers = []
   end
 
   def add_backer(backer)
     @backers << backer
+    backer.back_project(self)
   end
 
 end
