900c9a3d0e055ebe852e593a4dcb355159787ad7
Dean L Betty <deanlloyd64@gmail.com>
2019-09-25 17:57:14 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+  attr_reader :name, :backed_projects
+   
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+     
+  end
+  
+  def back_project(project)
+    unless @backed_projects.include?(project)
+      @backed_projects << project
+      project.add_backer(self)
+    end
+  end
+   
+   
+end
